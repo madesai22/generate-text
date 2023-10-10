a16ef55c2400999688c163fa22529c20fdc8a96e
madesai22 <meera.ann.desai@gmail.com>
2023-10-10 14:01:57 -0400

    add

--- generate-hf.py
+++ generate-hf.py
@@ -13,6 +13,10 @@ from transformers import pipeline
 #encoded_input = tokenizer(text, return_tensors='pt')
 #output = model(**encoded_input
 
-unmasker = pipeline('fill-mask', model='bert-base-cased')
-example = unmasker("Hello I'm a [MASK] model.")
-print(example)
+#unmasker = pipeline('fill-mask', model='bert-base-cased')
+#example = unmasker("Hello I'm a [MASK] model.")
+
+generate = pipeline('text-generation', model='bert-base-cased')
+gen_example = unmasker("When I went there it was")
+
+print(gen_example)
