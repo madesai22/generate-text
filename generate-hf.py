136102a3ea916a6bec1e0b06d61a2df0968896dd
madesai22 <meera.ann.desai@gmail.com>
2023-10-10 13:37:17 -0400

    add generate

---
+++ generate-hf.py
@@ -0,0 +1,18 @@
+# import relevant stuff
+from transformers import pipeline
+#from transformers import BertTokenizer, BertModel
+
+# preprocess prompt text 
+#tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
+
+
+# get model 
+#model = BertModel.from_pretrained("bert-base-cased")
+
+#text = "Replace me by any text you'd like."
+#encoded_input = tokenizer(text, return_tensors='pt')
+#output = model(**encoded_input
+
+unmasker = pipeline('fill-mask', model='bert-base-cased')
+unmasker("Hello I'm a [MASK] model.")
+print(unmasker)
