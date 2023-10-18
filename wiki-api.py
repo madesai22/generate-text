4c1033386dc781af2b86f639e3a5f0e4f4db4fe7
madesai22 <meera.ann.desai@gmail.com>
2023-10-18 12:39:55 -0400

    add wiki

--- wiki-api.py
+++ wiki-api.py
@@ -4,10 +4,20 @@ import wikipediaapi
 def print_categories(page):
         categories = page.categories
         for title in sorted(categories.keys()):
+            print(title)
             print("%s: %s" % (title, categories[title]))
 
 def check_exists(page):
     print("Page - Exists: %s" % page.exists())
+
+def print_categorymembers(categorymembers, level=0, max_level=1):
+        for c in categorymembers.values():
+            print("%s: %s (ns: %d)" % ("*" * (level + 1), c.title, c.ns))
+            if c.ns == wikipediaapi.Namespace.CATEGORY and level < max_level:
+                print_categorymembers(c.categorymembers, level=level + 1, max_level=max_level)
+#def get_birth_year(page):
+#    categories = page.categories
+#    if 
 
     
 wiki_wiki = wikipediaapi.Wikipedia('GenerateText (madesai@umich.edu)', 'en')
