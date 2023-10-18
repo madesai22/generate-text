e103ebd659fc143c0ce664d8b70827d98d4e5f68
madesai22 <meera.ann.desai@gmail.com>
2023-10-18 13:42:08 -0400

    add wiki

--- wiki-api.py
+++ wiki-api.py
@@ -12,7 +12,6 @@ def check_exists(page):
 
 def print_categorymembers(categorymembers, level=0, max_level=1):
         for c in categorymembers.values():
-            print(c.title)
             print("%s: %s (ns: %d)" % ("*" * (level + 1), c.title, c.ns))
             if c.ns == wikipediaapi.Namespace.CATEGORY and level < max_level:
                 print_categorymembers(c.categorymembers, level=level + 1, max_level=max_level)
@@ -42,13 +41,14 @@ def get_death_year(page):
     return death_year
 
 wiki_wiki = wikipediaapi.Wikipedia('GenerateText (madesai@umich.edu)', 'en')
-page_py = wiki_wiki.page('Dennis Adams_(boxer)')
-cat = wiki_wiki.page("Category:1971 deaths")
+#page_py = wiki_wiki.page('Dennis Adams_(boxer)')
+cat = wiki_wiki.page("Category:1930 deaths")
 
-birth_year = get_birth_year(page_py)
-death_year = get_death_year(page_py)
+#birth_year = get_birth_year(page_py)
+#death_year = get_death_year(page_py)
 
 print("birth year: {}, death year: {}".format(birth_year,death_year))
 
-print_categorymembers(cat.categorymembers)
+
 print(get_category_members(cat.categorymembers))
+print(len(get_category_members))
