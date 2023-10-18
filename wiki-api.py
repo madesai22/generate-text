81dad4e2328135c8714acb64ffba7117a47329c8
madesai22 <meera.ann.desai@gmail.com>
2023-10-18 12:34:37 -0400

    add wiki

--- wiki-api.py
+++ wiki-api.py
@@ -1,7 +1,19 @@
 import wikipediaapi
+
+
+def print_categories(page):
+        categories = page.categories
+        for title in sorted(categories.keys()):
+            print("%s: %s" % (title, categories[title]))
+
+def check_exists(page):
+    print("Page - Exists: %s" % page.exists())
+
     
 wiki_wiki = wikipediaapi.Wikipedia('GenerateText (madesai@umich.edu)', 'en')
-page_py = wiki_wiki.page('Python_(programming_language)')
+page_py = wiki_wiki.page('Dennis Adams_(boxer)')
 
-page_py = wiki_wiki.page('Python_(programming_language)')
-print("Page - Exists: %s" % page_py.exists())
+print_categories(page_py)
+
+
+
