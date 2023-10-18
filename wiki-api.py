20e21e91a8adacc7c3d2e4b041d67f4a2ccb51d4
madesai22 <meera.ann.desai@gmail.com>
2023-10-18 13:13:30 -0400

    add wiki

--- wiki-api.py
+++ wiki-api.py
@@ -15,15 +15,31 @@ def print_categorymembers(categorymembers, level=0, max_level=1):
             print("%s: %s (ns: %d)" % ("*" * (level + 1), c.title, c.ns))
             if c.ns == wikipediaapi.Namespace.CATEGORY and level < max_level:
                 print_categorymembers(c.categorymembers, level=level + 1, max_level=max_level)
-#def get_birth_year(page):
-#    categories = page.categories
-#    if 
+def YEAR_NOT_FOUND():
+    return -1999
 
-    
+def get_birth_year(page):
+    birth_year = YEAR_NOT_FOUND()
+    categories = page.categories
+    for title in sorted(categories.keys()):
+        if re.findall("Category:\d{4}\sbirths",text): year = int(re.findall("\d{4}", text)[0])
+    return birth_year
+
+def get_death_year(page):
+    death_year = YEAR_NOT_FOUND()
+    categories = page.categories
+    for title in sorted(categories.keys()):
+        if re.findall("Category:\d{4}\sdeaths",text): year = int(re.findall("\d{4}", text)[0])
+    return death_year
+
 wiki_wiki = wikipediaapi.Wikipedia('GenerateText (madesai@umich.edu)', 'en')
 page_py = wiki_wiki.page('Dennis Adams_(boxer)')
 
-print_categories(page_py)
+birth_year = get_birth_year(page_py)
+death_year = get_death_year(page_py)
 
+print("birth year: {}, death year: {}")
+
+print_categorymembers("Category:1971 deaths")
 
 
