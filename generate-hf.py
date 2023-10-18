80d87a85350c743c6214049a2e4d2f604138d42d
Meera Desai <madesai@m-fvfg615gq6lw.local>
2023-10-18 16:08:36 -0400

    add wiki

--- generate-hf.py
+++ generate-hf.py
@@ -1,5 +1,15 @@
 # import relevant stuff
 from transformers import pipeline
+
+def fill_mask(prompt, model='bert-base-cased'):
+    unmasker = pipeline('fill-mask', model=model)
+    response = unmasker(prompt)
+    print(response)
+
+prompt = "Theodore Roosevelet was born in the year [MASK]"
+
+fill_mask(prompt)
+
 #from transformers import BertTokenizer, BertModel
 
 # preprocess prompt text 
@@ -16,7 +26,7 @@ from transformers import pipeline
 #unmasker = pipeline('fill-mask', model='bert-base-cased')
 #example = unmasker("Hello I'm a [MASK] model.")
 
-generate = pipeline('text-generation', model='bert-base-cased',is_decoder=True)
-gen_example = generate("When I went there it was")
+#generate = pipeline('text-generation', model='bert-base-cased',is_decoder=True)
+#gen_example = generate("When I went there it was")
 
-print(gen_example)
+#print(gen_example)
