2d650926397495ecd5a7052c28f729ebc1c2b07f
Meera Desai <madesai@m-fvfg615gq6lw.local>
2023-10-24 19:52:20 -0400

    add newline

--- read-textbooks.py
+++ read-textbooks.py
@@ -8,24 +8,28 @@ out_file = open("./questions.csv","w")
 out_file.write("question,file,page\n")
 question_list = []
 
-for f in os.listdir(path):
-    print(f)
-    if f.endswith(".pdf"):
-        reader = PdfReader(path+f)
-        page_number = 1
-        for page in reader.pages[25:93]:
-            raw_text = page.extract_text()
-            clean_text = pp.remove_whitespaces(raw_text)
-            questions = pp.find_questions(clean_text)
-            print(questions)
-            if questions: 
-                for q in questions:
-                    out_file.write("{},{},{}\n".format(q,f,page_number))
- #           if questions:
- #               question_list += questions
-            page_number += 1 
-out_file.close()
-#print(question_list)
+reader = PdfReader(path+"HSWorld.pdf")
+page = reader.pages[32]
+print(page)
+
+# for f in os.listdir(path):
+#     print(f)
+#     if f.endswith(".pdf"):
+#         reader = PdfReader(path+f)
+#         page_number = 1
+#         for page in reader.pages[25:93]:
+#             raw_text = page.extract_text()
+#             clean_text = pp.remove_whitespaces(raw_text)
+#             questions = pp.find_questions(clean_text)
+#             print(questions)
+#             if questions: 
+#                 for q in questions:
+#                     out_file.write("{},{},{}\n".format(q,f,page_number))
+#  #           if questions:
+#  #               question_list += questions
+#             page_number += 1 
+# out_file.close()
+# #print(question_list)
 
 
 
