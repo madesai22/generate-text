7fab53a4217760ef420f39fa2493092bb5cbaa66
Meera Desai <madesai@m-fvfg615gq6lw.local>
2023-10-30 20:47:42 -0400

    add test

---
+++ test-wiki-api.py
@@ -0,0 +1,17 @@
+import wikipediaapi
+# only english language pages
+api = wikipediaapi.Wikipedia('en')
+
+species = ["eschrichtius robustus"]
+
+wikiurls = []
+# loop through the unique species names
+for name in species:
+    # Spaces to be replaced with underscore for Wiki's API
+    name = name.replace(" ", "_")
+    p = api.page(name)
+    
+    # Store the url retrieved and name we called with
+    data = {'url': p.fullurl, 'scientific_name': name}
+    # Append dictionary to list
+    wikiurls.append(data)
