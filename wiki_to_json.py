79790cbca9fd74273834503b6eb79844537ee53e
Meera Desai <madesai@m-fvfg615gq6lw.local>
2023-11-01 16:12:48 -0400

    change file saving structure

--- wiki_to_json.py
+++ wiki_to_json.py
@@ -129,7 +129,7 @@ def main():
          born_after = int(items[2])
          file_name = category.partition(':')[2].lower().replace(" ","_")
          print(category,file_name)
-         wiki_cat = wiki_wiki.page(c)
+         wiki_cat = wiki_wiki.page(category)
          category_members = get_category_members(wiki_cat.categorymembers)
          data = make_dictionary(category_members,category=category.partition(':')[2],clean=True,born_before=born_before)
          print("{} items in {}.".format(len(data), category))
