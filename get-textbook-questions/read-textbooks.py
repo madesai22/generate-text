8c1f63c86c50b5f7d57d76e64cc5781dd78b3aa2
Meera Desai <madesai@m-fvfg615gq6lw.local>
2023-11-02 08:55:11 -0400

    changed file structure

--- get-textbook-questions/read-textbooks.py
+++ get-textbook-questions/read-textbooks.py
@@ -1,8 +1,30 @@
 from PyPDF2 import PdfReader
-import preprocess as pp
 import os
 import re
 
+def remove_whitespaces(text,paragraph=False):
+    if paragraph:
+       return re.sub(' +|\t+', ' ', text)
+    else:
+       end_chars = re.sub('\n','',text)
+       return re.sub(' +|\t+', ' ', end_chars)
+    
+def find_questions(text):
+    # find question area:
+    test = re.findall("QUESTIONS TO GUIDE INQUIRY",text)
+    if test:
+        print("test!!:{}".format(test))
+        print("text:{}".format(text))
+        question_area = re.findall("QUESTIONS TO GUIDE INQUIRY\s*.*[0-9]\.\s.*\?",text)[0]
+        question_area = re.sub("QUESTIONS TO GUIDE INQUIRY\s*","",question_area) # remove header
+
+        # split questions:
+        question_list = re.split("\s?[0-9]+\.\s", question_area)
+        question_list = list(filter(None, question_list)) # remove empty items
+        return question_list
+    else:
+        return None
+    
 path = "/data/madesai/history-llm-data/mi-open-textbooks/"
     
 files = ["HSUSFull.pdf","HSWorld.pdf"]
@@ -12,8 +34,8 @@ for f in files:
     file_questions = set()
     for page in reader.pages:
         raw_text = page.extract_text()
-        clean_text = pp.remove_whitespaces(raw_text)
-        questions = pp.find_questions(clean_text)
+        clean_text = remove_whitespaces(raw_text)
+        questions = find_questions(clean_text)
         print(questions)
         
         if questions: 
