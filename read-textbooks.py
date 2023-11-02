7c30acc2169a81435353ee75c30f67a41af9cf31
Meera Desai <madesai@m-fvfg615gq6lw.local>
2023-11-02 08:49:24 -0400

    add read-textbooks.py

--- read-textbooks.py
+++ read-textbooks.py
@@ -4,37 +4,24 @@ import os
 import re
 
 path = "/data/madesai/history-llm-data/mi-open-textbooks/"
-#out_file = open(path + "questions.csv","w")
-out_file = open("./questions.csv","w")
-out_file.write("question,file,page\n")
-question_list = []
-
-reader = PdfReader(path+"HSWorld.pdf")
-for page in reader.pages:
-    raw_text = page.extract_text()
-    cleaned = pp.remove_whitespaces(raw_text)
-    questions = re.findall("\s[1-9]\.\D.*?\?",cleaned)
-    print(questions)
     
-
-# for f in os.listdir(path):
-#     print(f)
-#     if f.endswith(".pdf"):
-#         reader = PdfReader(path+f)
-#         page_number = 1
-#         for page in reader.pages[25:93]:
-#             raw_text = page.extract_text()
-#             clean_text = pp.remove_whitespaces(raw_text)
-#             questions = pp.find_questions(clean_text)
-#             print(questions)
-#             if questions: 
-#                 for q in questions:
-#                     out_file.write("{},{},{}\n".format(q,f,page_number))
-#  #           if questions:
-#  #               question_list += questions
-#             page_number += 1 
-# out_file.close()
-# #print(question_list)
+files = ["HSUSFull.pdf","HSWorld.pdf"]
+for f in files:
+    reader = PdfReader(path+f)
+    out_file = f[:-4]+".csv"
+    file_questions = set()
+    for page in reader.pages:
+        raw_text = page.extract_text()
+        clean_text = pp.remove_whitespaces(raw_text)
+        questions = pp.find_questions(clean_text)
+        print(questions)
+        
+        if questions: 
+            for q in questions:
+                file_questions.add(questions)
+    for q in file_questions:
+        out_file.write(q+"\n")
+    out_file.close()
 
 
 
