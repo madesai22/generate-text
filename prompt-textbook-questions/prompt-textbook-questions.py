2c2150ea8fb41ca5438078199772e45299d13dae
Meera Desai <madesai@m-fvfg615gq6lw.local>
2023-12-07 14:21:58 -0500

    add

--- prompt-textbook-questions/prompt-textbook-questions.py
+++ prompt-textbook-questions/prompt-textbook-questions.py
@@ -30,7 +30,7 @@ def initiate_gpt2(medium = False):
 
 def gpt2_text_to_text(prompt, model, tokenizer):
     input_ids = tokenizer(prompt, return_tensors='pt').input_ids.to("cuda")
-    outputs = model.generate(input_ids, pad_token_id=tokenizer.eos_token_id, max_new_tokens=300,do_sample=True)
+    outputs = model.generate(input_ids, pad_token_id=tokenizer.eos_token_id, max_new_tokens=300,temperature = 0.9,do_sample=True)
     return (tokenizer.decode(outputs[0], skip_special_tokens=True))
 
 
@@ -52,19 +52,19 @@ def main():
     question_fname = ["HSUS.txt","HSWorld_clean.txt"]
     path_to_questions = "/home/madesai/generate-text/get-textbook-questions/"
     #model,tokenizer = initiate_flan5_text_to_text(xxl=True)
-    #model, tokenizer = initiate_gpt2()
+    model, tokenizer = initiate_gpt2()
     set_seed(42)
 
     response_dict = {"Question":[],"Response":[]}
 
     test = 0 
     for qf in question_fname:
-        outfile = open(qf[:-4]+"-gpt2-default-response.csv","w")
+        outfile = open(qf[:-4]+"-gpt2-custom-temp-response.csv","w")
         question_file = open(path_to_questions+qf,"r")
         for prompt in question_file:
-            response = gpt_2_generate(prompt)[0]["generated_text"]
+        #    response = gpt_2_generate(prompt)[0]["generated_text"]
       
-            #response = gpt2_text_to_text(prompt,model,tokenizer)
+            response = gpt2_text_to_text(prompt,model,tokenizer)
             #response = flant5_text_to_text(prompt,model,tokenizer)
             response = strip_repsonse(response)
             response_dict["Question"].append(prompt)
