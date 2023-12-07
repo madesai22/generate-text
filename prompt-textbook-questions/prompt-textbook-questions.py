cab69c7e4f28ee5ccf2a30cdaaa67299df545438
Meera Desai <madesai@m-fvfg615gq6lw.local>
2023-12-07 10:31:29 -0500

    add

--- prompt-textbook-questions/prompt-textbook-questions.py
+++ prompt-textbook-questions/prompt-textbook-questions.py
@@ -18,6 +18,14 @@ def flant5_text_to_text(prompt, model,tokenizer):
     outputs = model.generate(input_ids)
     return(tokenizer.decode(outputs[0]))
 
+def gpt_2_generate(prompt):
+    generator = pipeline('text-generation', model='gpt2')
+    set_seed(42)
+    response = generator(prompt, max_length=300, num_return_sequences = 3)
+    return response
+
+
+
 def strip_repsonse(text):
     text = re.sub("<pad> ","",text)
     text = re.sub("</s>","",text)
@@ -28,25 +36,29 @@ def strip_repsonse(text):
 def main():
     question_fname = ["HSUSFull.txt","HSWorld_clean.txt"]
     path_to_questions = "/home/madesai/generate-text/get-textbook-questions/"
-    model,tokenizer = initiate_flan5_text_to_text(xxl=True)
+    #model,tokenizer = initiate_flan5_text_to_text(xxl=True)
+    
 
     response_dict = {"Question":[],"Response":[]}
 
     test = 0 
     for qf in question_fname:
-        outfile = open(qf[:-4]+"-flant5-response.csv","w")
+        outfile = open(qf[:-4]+"-gpt2-response.csv","w")
         question_file = open(path_to_questions+qf,"r")
         for prompt in question_file:
-            response = flant5_text_to_text(prompt,model,tokenizer)
+            gpt_2_generate(prompt)
+            #response = flant5_text_to_text(prompt,model,tokenizer)
             response = strip_repsonse(response)
             response_dict["Question"].append(prompt)
             response_dict["Response"].append(response)
             
-            print(prompt)
-            print(response)
-           # if test > 9:
+            if test < 9:
+                print(prompt)
+                print(response)
+                break
+           
             df = pd.DataFrame(response_dict)
-                #print(df)
+             
             df.to_csv(outfile,sep=";")
             test += 1
             if test %10 == 0:
