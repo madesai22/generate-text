f77b0b3ec0943ddd5a290f6d3aa38da9eee7323d
Meera Desai <madesai@m-fvfg615gq6lw.local>
2023-12-07 07:48:16 -0500

    add questions

--- get-textbook-questions/read-textbooks.py
+++ get-textbook-questions/read-textbooks.py
@@ -23,9 +23,9 @@ def pre_process_sentence(text, stopwords=None):
     for i in range(len(text)):
        sentence = text[i]
        if sentence is not None:
-          sentence = strip_punctuation(sentence)
+          #sentence = strip_punctuation(sentence)
           sentence = remove_whitespaces(sentence)
-          sentence = strip_punctuation(sentence).lower().strip()
+          #sentence = strip_punctuation(sentence).lower().strip()
           new_text.append(sentence)
     return new_text
     
