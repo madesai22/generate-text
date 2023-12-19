cf158af944a3d92db0af68accd154c600c52fb91
Meera Desai <madesai@0587662925.vpn.umich.net>
2023-12-19 17:37:29 -0500

    add

--- predict-birthyear/open_json.py
+++ predict-birthyear/open_json.py
@@ -6,7 +6,7 @@ from numba import jit, cuda
 #@jit(target_backend='cuda')
 def open_files():
     directory = "/data/madesai/history-llm-data/wikipedia-json-files/"
-    filename = "all_wiki.json.json"
+    filename = "all_wiki.json"
     #for filename in os.listdir(directory):
     f = os.path.join(directory, filename)
     data = fh.read_json(f)
