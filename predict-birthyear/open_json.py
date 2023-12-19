efa90be945b319e73bbe5aea79035abf6e2ec580
Meera Desai <madesai@m-fvfg615gq6lw.local>
2023-12-19 13:43:30 -0500

    add

--- predict-birthyear/open_json.py
+++ predict-birthyear/open_json.py
@@ -3,13 +3,13 @@ import os
 import random 
 from numba import jit, cuda
 
-@jit(target_backend='cuda')
+#@jit(target_backend='cuda')
 def open_files():
     directory = "/data/madesai/history-llm-data/wikipedia-json-files"
 
     for filename in os.listdir(directory):
         f = os.path.join(directory, filename)
-        data = fh.read_jsonlist(f)
+        data = fh.read_json(f)
         print(random.sample(dict.items,10))
 
 if __name__=="__main__":
--- predict-birthyear/run-generations.sh
+++ predict-birthyear/run-generations.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 export CUDA_VISIBLE_DEVICES=1
-python wiki_to_json.py
+python open_json.py
