63776a9657134c69e8cc30a6407a15d7051def05
Meera Desai <madesai@m-fvfg615gq6lw.local>
2023-12-20 17:59:41 -0500

    add

---
+++ predict-birthyear/analyze_birth_predictions.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def make_hist(data,x,title,binwidth=None,bins=None,kde=False):
+    return sns.displot(data,x=x,binwidth=binwidth,bins=bins,kde=kde).set(title=title)
+
+
+def main ():
+    path = "temp.csv"
+    data = pd.read_csv(path)
+    save_path = "./plots/"
+
+    # accuracy distribution 
+    acc_plot = make_hist(data,"Years off")
+    
+
+
