3d4d06a77fb914d95aa953beccb9043d8860e79c
Meera Desai <madesai@m-fvfg615gq6lw.local>
2023-12-20 14:46:21 -0500

    add

--- predict-birthyear/get_random_sample.py
+++ predict-birthyear/get_random_sample.py
@@ -11,15 +11,18 @@ from transformers import pipeline, set_seed
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 import pandas as pd
+import datetime
 
 def initiate_flan5_text_to_text(xxl = False):
     if xxl: 
          tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-xxl")
          model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-xxl", device_map="auto")
+         model_string = "flant5xxl"
     else:
          tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-base")
          model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-base", device_map="auto") 
-    return model, tokenizer
+         model_string = "flant5base"
+    return model, tokenizer, model_string
 
 def flant5_text_to_text(prompt, model,tokenizer):
     input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to("cuda")
@@ -30,13 +33,16 @@ def initiate_gpt2(medium = False, large = False):
     if medium: 
         model = GPT2LMHeadModel.from_pretrained("gpt2-medium",device_map="auto")
         tokenizer = GPT2Tokenizer.from_pretrained("gpt2-medium")
+        model_string = "gpt2medium"
     elif large:
         model = GPT2LMHeadModel.from_pretrained("gpt2-large",device_map="auto")
         tokenizer = GPT2Tokenizer.from_pretrained("gpt2-large")
+        model_string = "gpt2large"
     else:
        tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-       model = GPT2LMHeadModel.from_pretrained("gpt2",device_map="auto")    
-    return model, tokenizer
+       model = GPT2LMHeadModel.from_pretrained("gpt2",device_map="auto")  
+       model_string = "gpt2"  
+    return model, tokenizer, model_string
 
 def gpt2_text_to_text(prompt, model, tokenizer):
     input_ids = tokenizer(prompt, return_tensors='pt').input_ids.to("cuda")
@@ -51,6 +57,7 @@ def gpt_2_generate(prompt):
     response = generator(prompt, max_new_tokens=300, num_return_sequences = 1)
     return response
 
+# prompt functions
 def make_prompt(prompt_form, name, clean = False):
      if clean:
           name = re.sub(r'\([^)]*\)', '', name)
@@ -67,6 +74,7 @@ def strip_repsonse(text):
     text = re.sub("</s>","",text)
     return text
 
+# data function
 def prep_random_sample(data_path,wiki_wiki,size,percent=False):
     #random_keys = fh.read_json_random_sample(data_path,size=.2,percent=True,return_keys= True)
     random_sample_dict = {}
@@ -88,6 +96,7 @@ def prep_random_sample(data_path,wiki_wiki,size,percent=False):
         fh.write_to_json(all_data,data_path)
         return keys, random_sample_dict
 
+# prediction 
 def predict_birth_year(data, model, tokenizer, prompt_form):
     df_dict = {'Name':[],'True birth year': [], 'Pageviews':[],'Predicted birth year':[], "Years off": [], "Full response": []}
     for name in data.keys():
@@ -113,12 +122,21 @@ def predict_birth_year(data, model, tokenizer, prompt_form):
         df_dict['Full response'].append(response)
     return df_dict
 
+# organizaiton 
 def record_seen_keys(keys, outfile):
     if os.path.exists(outfile):
         seen_keys = fh.unpickle_data(outfile)
         fh.pickle_data(seen_keys + keys,outfile)
     else:
         fh.pickle_data(keys,outfile)
+
+def get_save_path(args, HEAD):
+    date = '{}'.format( datetime.datetime.now().strftime('%Y-%m-%d-%H-%M') )
+    seedstr = str(args.seed).zfill(3)
+    suffix = "{}_{}_{}_{}".format(args.alg, date, seedstr)
+    result_path = os.path.join(HEAD, suffix)
+    return result_path
+
 
 def main(): # parameters are: data_path, size, model + model parameters, prompt_form, outpath, csv outpath, seed 
     set_seed(42)
@@ -127,12 +145,22 @@ def main(): # parameters are: data_path, size, model + model parameters, prompt_
     csv_out = "temp.csv"
     keys_out = "seen_keys.pkl"
     prompt_form = "What year was {} born?"
-    model,tokenizer = initiate_flan5_text_to_text(xxl=True)
-    keys, data = prep_random_sample(data_path,wiki_wiki,size=.001,percent=True)
-    record_seen_keys(keys, keys_out)
-    pred_dict = predict_birth_year(data,model,tokenizer,prompt_form)
-    df = pd.DataFrame(pred_dict)
-    df.to_csv(csv_out,sep=";")
+    model,tokenizer, model_string = initiate_flan5_text_to_text(xxl=True)
+    
+    sample_size = 0.001
+    percent = True
+    keys, data = prep_random_sample(data_path,wiki_wiki,size=sample_size,percent=percent)
+    # save parameters
+    f = open("temp_log.csv","w")
+    f.write("model:{}\n".format(model_string))
+    f.write("sample size: {}\n".format(str(sample_size)))
+    f.write("percent: {}\n".format(str(percent)))
+    f.write("prompt form:{}\n".format(prompt_form))
+
+    # record_seen_keys(keys, keys_out)
+    # pred_dict = predict_birth_year(data,model,tokenizer,prompt_form)
+    # df = pd.DataFrame(pred_dict)
+    # df.to_csv(csv_out,sep=";")
 
 if __name__ == "__main__":
     main()
