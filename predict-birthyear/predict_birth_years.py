e0753c30a6a5303c7f2b2d5c048100b84e43c995
Meera Desai <madesai@0587672648.vpn.umich.net>
2023-12-21 08:32:28 -0500

    add

--- predict-birthyear/predict_birth_years.py
+++ predict-birthyear/predict_birth_years.py
@@ -229,5 +229,5 @@ def main(): # parameters are: data_path, size, model + model parameters, prompt_
 if __name__ == "__main__":
     model, tokenizer, model_string = initiate_falcon()
     prompt = "What year was Paris Hilton born?"
-    response = falcon_text_to_text(prompt,model,tokenizer)
+    response = gpt2_text_to_text(prompt,model,tokenizer)
     print(response)
