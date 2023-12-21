a15748abfe64e358d6584f2d0e5599225545d3c1
Meera Desai <madesai@m-fvfg615gq6lw.local>
2023-12-21 13:54:05 -0500

    add

--- predict-birthyear/analyze_birth_predictions.py
+++ predict-birthyear/analyze_birth_predictions.py
@@ -102,7 +102,7 @@ def main ():
         ax = sns.displot(years_off)
        # ax = sns.displot(data,x="Years off") 
         ax.fig.subplots_adjust(top=.95)
-        ax.set(xlabel='Years off')
+        ax.set(xlabel='Abs(prediction-true) (years)')
         ax.set(title = model_string+" accuracy")
         plt.savefig(save_path+"_accuracy_hist.jpg")
 
@@ -123,6 +123,7 @@ def main ():
         ax = sns.relplot(data, x="Years off", y="Pageviews")
         ax.fig.subplots_adjust(top=.95)
         ax.set(title=model_string+" accuracy vs page views")
+        ax.set(xlabel='Abs(prediction-true) (years)')
         plt.savefig(save_path+"_acc_v_page_views.jpg")
         plt.close()
 
@@ -130,6 +131,7 @@ def main ():
         ax = sns.relplot(data, x="Years off", y="True birth year")
         ax.fig.subplots_adjust(top=.95)
         ax.set(title=model_string+" accuracy vs true birth year")
+        ax.set(xlabel='Abs(prediction-true) (years)')
         plt.savefig(save_path+"_acc_v_true_by.jpg")
         plt.close()
 
